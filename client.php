0ba9778c44dbe4180dca3c2adb826d6c125b78d2
EsmeraldaACar <esmeralda.arista.isc@gmail.com>
2020-10-21 16:18:22 -0500

    Funcionalidad nueva agregada

--- client.php
+++ client.php
@@ -7,4 +7,21 @@ for ($i=0; $i < $a; $i++) {
     $a++;
 }
 
+//Funcionalidad realizada con un feature en GitFlow
+    switch($dia){
+        case 1: echo "Lunes";
+        break;
+        case 2: echo "Martes";
+        break;
+        case 3: echo "Miercoles";
+        break;
+        case 4: echo "Jueves";
+        break;
+        case 5: echo "Viernes";
+        break;
+        case 6: echo "Sabado";
+        break;
+        case 7: echo "Domingo";
+        break;      
+    }
 ?>
